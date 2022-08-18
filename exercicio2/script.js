e4d2d7c39c1049ebf246a34fcc698b7ba46f2622
DavidAlves28 <dav.alvesc28@gmail.com>
2022-08-18 20:00:19 -0300

    Barbosa-DavidAlves-Lacos

---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+
+let num =Number(prompt("Digite um numero de 1 a 10"));
+
+let array = [1,2,3,4,5,6,7,8,9,10,11]
+
+console.log(`a Tabuada do ${num} é`);
+
+for (i in array){
+    console.log(`${num}` `x` `${i}`= `${num*i}`);
+}
